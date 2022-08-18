5352a542ae304de34d6fc96b100cab6603fd6987
Tucker Steil <tuckersteil@gmail.com>
2022-08-17 17:44:50 -0700

    commit tree 5a094119e17dad18b2f45e5339faf92da43f4f7d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word 
+    def initialize(word)
+        @word = word 
+    end 
+
+    def match(arr)
+        it_matches = []
+        arr.map do |indv_word|
+            sort_chars = indv_word.chars.sort
+            sort_word = @word.chars.sort
+            if sort_word == sort_chars
+                it_matches.push(indv_word)
+            end
+        end 
+        it_matches
+    end 
+end
